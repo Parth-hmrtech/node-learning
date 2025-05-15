938f7d6cd389fe6d96c7ab534bc448651dec53db
Parth-hmrtech <pk.hmrtech@gmail.com>
2025-05-15 15:52:50 +0530

    create post request

--- node-learning-express/node-learning-middleware.js
+++ node-learning-express/node-learning-middleware.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+let item=[];
+app.post('/login',(req,res)=>{
+    const {name} = req.body;
+    if(!name)
+    {
+        return res.status(400).json({error:'Name is required'})
+    }
+    item.push(name  );
+    res.status(201).json({ message: 'Name added successfully', data: name });
+
+});
 app.get('/home',(req,res)=>{
-    res.send("home");
+    res.json(item)
     res.end();
 });
-
-app.listen(3006);
+app.listen(3009);

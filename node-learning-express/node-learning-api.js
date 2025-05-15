17ee666b33188ef082db572088ab3319f8ce0209
Parth-hmrtech <pk.hmrtech@gmail.com>
2025-05-15 15:03:02 +0530

    pull request

--- node-learning-express/node-learning-api.js
+++ node-learning-express/node-learning-api.js
@@ -9,13 +9,6 @@ app.get('/login', (req, res) => {
     res.send(' login page');
 
 });
-app.get('/about', (req, res) => {
-    res.send('about page');
-});
-app.get('/help',(req,res)=>{
-    res.send('help page')
-});
-
 app.listen(3002, () => {
     console.log('Server running on http://localhost:3001');
 });

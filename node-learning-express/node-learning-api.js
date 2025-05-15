347d8d9a99963d9974ad3281a5af88fde613d5e8
Parth-hmrtech <pk.hmrtech@gmail.com>
2025-05-15 15:15:47 +0530

    my new update

--- node-learning-express/node-learning-api.js
+++ node-learning-express/node-learning-api.js
@@ -5,16 +5,6 @@ app.get('/', (req, res) => {
     res.send('home page');
 });
 
-app.get('/login', (req, res) => {
-    res.send(' login page');
-
-});
-app.get('/about', (req, res) => {
-    res.send('about page');
-});
-app.get('/help',(req,res)=>{
-    res.send('help page')
-});
 
 app.listen(300, () => {
     console.log('Server running on http://localhost:3001');
